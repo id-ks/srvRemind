ac34c96c36100058d0dd5c5a0b46c5617c002be2
ks <architectks@yandex.ru>
2018-05-31 00:12:15 +0300

    Second

---
+++ src/main/java/pack/main/config/WebConfig.java
@@ -0,0 +1,13 @@
+package pack.main.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("pack.main")
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+}
---
+++ src/main/java/pack/main/controller/ReminderController.java
@@ -0,0 +1,18 @@
+package pack.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+@RequestMapping("/reminder")
+public class ReminderController {
+
+    @RequestMapping(value = "/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getReminder() {
+        return "remind";
+    }
+}
